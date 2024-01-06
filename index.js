ff35199232aa944d99db9bb1afd10a0f56121937
YankovWeb <yankovWeb@outlook.com>
2024-01-06 11:47:28 +0200

    1000 create function sum

---
+++ index.js
@@ -0,0 +1,16 @@
+// a + b
+function sum(a, b) {
+  console.log(a + b);
+}
+
+
+function addOne(arg) {
+  return arg + 1
+}
+//undefined
+function sum2(a, b, addOne) {
+  return addOne(a + b);
+}
+
+
+
